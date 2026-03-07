0512d8df6df5f3bd205363ef020601260dad6dec
TheStin777 <king-ice-spider@proton.me>
2026-03-07 10:48:26 +0900

    added api key for google Tested the output

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-def main():
-    print("Hello from ai-agent!")
+import os
+from google import genai
+from dotenv import load_dotenv
 
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
 
-if __name__ == "__main__":
-    main()
+response = client.models.generate_content(
+    model='gemini-2.5-flash',
+    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+   
+)
+
+print(response.text)
