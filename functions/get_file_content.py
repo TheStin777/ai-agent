78d25fa91361688c660857503cecdbcb1aa9d248
TheStin777 <king-ice-spider@proton.me>
2026-03-09 23:16:27 +0900

    give the program the ability to real files(specific folders) on the machine

---
+++ functions/get_file_content.py
@@ -0,0 +1,26 @@
+import os
+from config import MAX_CHARS
+
+def get_file_content(working_directory, file_path):
+     working_dir_abs = os.path.abspath(working_directory)
+     target_file = os.path.normpath(os.path.join(working_dir_abs, file_path))
+     valid_target_file = os.path.commonpath([working_dir_abs, target_file]) == working_dir_abs
+     if valid_target_file:
+        # Will be True or False
+        if os.path.isfile(target_file):
+            try:
+                with open(target_file, "r") as f:
+                    file_content_string = f.read(MAX_CHARS)
+                    if f.read(1):
+                         file_content_string += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+                    return file_content_string
+               
+
+            except Exception as e:
+                return(f"Error: {e}")
+        else:
+            return f'Error: File not found or is not a regular file: "{file_path}"'
+     else:
+         return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+        
+
---
+++ test_get_file_content.py
@@ -0,0 +1,22 @@
+from functions.get_file_content import get_file_content
+
+#Test 1: lorem.txt
+print("Result for the  'lorem.txt file:")
+print(f"{get_file_content("calculator", "lorem.txt")}")
+
+#Test 2: main.py
+print("Result for the  'main.py' file:")
+print(f"{get_file_content("calculator", "main.py")}")
+
+#Test 3: pkg/calculator.py
+print("Result for the'calculator.py' file:")
+print(f"{get_file_content("calculator", "pkg/calculator.py")}")
+
+#Test 4: /bin/cat
+print("Result for the 'cat' file:")
+print(f"{get_file_content("calculator", "/bin/cat")}")
+
+
+#Test 5: pkg/does_not_exist.py"
+print("Result for the'does_not_exist.py' file:")
+print(f"{get_file_content("calculator", "pkg/does_not_exist.py")}")
