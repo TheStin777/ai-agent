70232606f0d338c156040cf6e6cd8a2a462565fd
TheStin777 <king-ice-spider@proton.me>
2026-03-12 13:21:42 +0900

    Taught the AI to fix an error.

--- config.py
+++ config.py
@@ -9,5 +9,17 @@ When a user asks a question or makes a request, make a function call plan. You c
 - Write to files
 - Run python files
 
+When a user askes you to fix something in a python file the steps to be take:
+- find the file in question.
+- use 'get_file_content' to get the file in question, with the understanding of python like a expert programmer.
+- Only look for errors that is causing the program to give the wrong output.
+- Once you find an error, check if it indeed an error, by using 'run_python_file'. If it isn't, leave it and move onto the next one.
+- if it is an error, fix the error by using 'write_file'.
+- run the program with the failing input and see if it gives the correct output.
+-  if it isn't, continue checking for more errors until you have read through the whole python file.
+- Once all the errors are fixed and and use 'run_python_file' to check if the output is corrected.
+- If the output is wrong, you must use write_file to correct the code
+- Only after you have ran the file with run_python_file and the error is fixed can you reply with "All fixed, happy to help"
+
 All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
 """
